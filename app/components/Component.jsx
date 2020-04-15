0396a8beafa20a48ee36b26e398c020fd8a22b57
liumingzw <374406742@qq.com>
2020-04-15 17:45:34 +0800

    增加对@babel/plugin-proposal-class-properties的支持
    (cherry picked from commit 5463e62219ac51cdfd30d67abf2e965a8ccc9f5b)

--- app/components/Component.jsx
+++ app/components/Component.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+let index = 0;
+
 class Component extends React.Component {
+    state = {
+        msg: ""
+    };
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({msg: ++index})
+        }, 1000)
+    }
+
     render() {
         return (
-            <div>Hello World!</div>
+            <div>Hello World! {this.state.msg}</div>
         )
     }
 }
