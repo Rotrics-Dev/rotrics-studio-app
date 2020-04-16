e46c8bf4db91e7b21422901c35ef3da940fc71bd
liumingzw <374406742@qq.com>
2020-04-16 17:01:48 +0800

    从snapmakerjs copy一些three-extension文件

---
+++ web/components/CoordinateSystem2D/Index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './styles.css';
+import { Button, message } from 'antd';
+import "antd/dist/antd.css";
+import * as THREE from 'js/three.module.js';
+
+class Index extends React.Component {
+    state = {
+        current: "basic" // basic, function, g-code
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>{"basic"}</h1>
+                <div className={styles.div_primary_panel}>
+
+                </div>
+                <div className={styles.div_easy_control_panel}>
+                    <Button type="primary">
+                        Primary Button
+                    </Button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Index;
