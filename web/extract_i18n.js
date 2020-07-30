444c23da75d3c8be6b68def9c9d4173f5753dd2d
liumingzw <374406742@qq.com>
2020-07-30 17:24:44 +0800

    refactor

--- web/extract_i18n.js
+++ web/extract_i18n.js
@@ -4,4 +4,4 @@ const extract_i18n_cura = require("./i18n_cura/extract");
 
 fs.rmdirSync("./build-web/asset/i18n", {recursive: true});
 extract_i18n_cura();
-jsonXlsx.xlsx2json("./translate.xlsx", "./build-web/asset/i18n/common/");
+jsonXlsx.xlsx2json("./i18n.xlsx", "./build-web/asset/i18n/common/");
