94c0592b2b18cced369cac5f8ff1fd615f0ef92a
tizif <kiixin@163.com>
2020-09-18 15:31:43 +0800

    版本更新，当软件发布12小时之后，软件端提醒用户下载更新，防止软件上传出错或其他人为出错之后没有时间修复

---
+++ web/src/components/NewVersionDetector/Index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import showdown from 'showdown';
+import checkUpdate from '../../utils/VersionUtils';
+import {Modal} from 'antd';
+
+class Index extends React.Component {
+    state = {}
+
+    componentDidMount() {
+        checkUpdate((latestVersionData) => {
+            this.setState({latestVersionData, showUpdate: true});
+        });
+    }
+
+    render() {
+        let info = '';
+        const {showUpdate, latestVersionData} = this.state;
+        if (showUpdate && latestVersionData && latestVersionData.infos.length > 0) {
+            const converter = new showdown.Converter()
+            info = converter.makeHtml(latestVersionData.infos[0].content);
+        }
+        console.log(info);
+        return (
+            <div>
+                {this.state.showUpdate && <Modal
+                    title={"New Version Rotrics Studio Is Available"}
+                    visible={this.state.showUpdate}
+                    onCancel={() => {
+                        this.setState({showUpdate: false});
+                    }}
+                    onOk={() => {
+                        this.setState({showUpdate: false});
+                        window.open('https://www.rotrics.com/pages/download', '_blank');
+                    }}>
+                    <div dangerouslySetInnerHTML={{__html: info}}/>
+                    <p>Download the latest Rotrics Studio now?</p>
+                </Modal>}
+            </div>
+        );
+    }
+}
+
+export default Index;
